9f99adbe25659a7c901812f9935aab5b1967cdff
Priscila Leite <pricila_leite@hotmail.com>
2024-07-03 21:44:13 -0300

    Número de Tentativas

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-alert('Boas vindas ao jogo do número secreto');
+alert('Bem-vindos ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
 let chute;
@@ -6,11 +6,10 @@ let tentativas = 1;
 
 // while o chute for diferente do que o n.S, faça:
 while (chute != numeroSecreto) {
-
     chute = prompt('Escolha um número entre 1 e 10');
-    // se chute for igual ao número secreto
+
+    // se chute for igual ao número secreto:
     if (chute == numeroSecreto) {
-        alert('Isso ai! Você descobriu, o número secreto é ' + numeroSecreto + ' com ' + tentativas + ' tentativas!');
     } else {
         if (chute > numeroSecreto) {
             alert('O número secreto é menor do que ' + chute + '!');
@@ -20,4 +19,12 @@ while (chute != numeroSecreto) {
         tentativas++;
     }
 }
+
+// Para caso positivo acertar na primeira tentativa, corrige o plural:
+if (tentativas > 1) {
+    alert('Isso ai! Você descobriu, o número secreto é ' + numeroSecreto + ' com ' + tentativas + ' tentativas!');
+} else {
+    alert('Isso ai! Você descobriu, o número secreto é ' + numeroSecreto + ' com ' + tentativas + ' tentativa!');
+}
+
 
